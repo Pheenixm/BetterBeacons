d2398e2a1bef5fbdea78d7a2e8952f68b5323532
MrTwiggy <skate_ty10@hotmail.com>
2013-01-20 22:52:38 -0500

    Added functionality for opening, initializing, and removing material from Beacon inventories.

--- BetterBeacons/src/com/pheenixm/betterbeacons/BetterBeacons.java
+++ BetterBeacons/src/com/pheenixm/betterbeacons/BetterBeacons.java
@@ -1,11 +1,17 @@
 package com.pheenixm.betterbeacons;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -31,6 +37,11 @@ public class BetterBeacons {
 		yMax = null;
 		zMin = null;
 		zMax = null;
+		
+		// TODO: Call initializeInventory(INVENTORY) if
+        // loading Beacon from file, whether INVENTORY is the
+        // previously saved copy of Beacon inventory
+		initializeInventory();
     }
 
 	BetterBeacons(
@@ -56,6 +67,11 @@ public class BetterBeacons {
 		instance = plugin;
 		beaconLocation = new Location(instance.getServer().getWorld(worldUuid), (double)x, (double)y, (double)z);
         setProperties(faction, radius, fuel_amount, fuel_material, positive, negative);
+        
+        // TODO: Call initializeInventory(INVENTORY) if
+        // loading Beacon from file, whether INVENTORY is the
+        // previously saved copy of Beacon inventory
+        initializeInventory();
 	}
 
 	public UUID getWorldUuid() {
@@ -132,6 +148,62 @@ public class BetterBeacons {
 		double dubZ = Math.pow(one.getZ() - two.getZ(), 2);
 		return (float) Math.sqrt(dubX + dubZ);
 	}
+	
+	/**
+	 * Initialize inventory using a pre-existing saved inventory copy
+	 */
+	private void initializeInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+	
+	/**
+	 * Initialize a brand new inventory for beacon
+	 */
+	private void initializeInventory() {
+		inventory = Bukkit.getServer().createInventory(null, inventoryRows*9, "Beacon Inventory");
+	}
+
+	/**
+	 * Opens the beacon inventory for given player
+	 */
+	public void openInventory(Player player) {
+		if (inventory != null)
+			player.openInventory(inventory);
+	}
+	
+	/**
+	 * Attempts to remove a specific material of given amount from Beacon inventory
+	 */
+	public boolean removeMaterial(int amount, Material material)
+	{
+		HashMap<Integer,? extends ItemStack> inventoryMaterials = inventory.all(material);
+		
+		int materialsToRemove = amount;
+		for(Entry<Integer,? extends ItemStack> entry : inventoryMaterials.entrySet())
+		{
+			if (materialsToRemove <= 0)
+				break;
+			
+			if(entry.getValue().getAmount() == materialsToRemove)
+			{
+				inventory.setItem(entry.getKey(), new ItemStack(Material.AIR, 0));
+				materialsToRemove = 0;
+			}
+			else if(entry.getValue().getAmount() > materialsToRemove)
+			{
+				inventory.setItem(entry.getKey(), new ItemStack(material, (entry.getValue().getAmount() - materialsToRemove)));
+				materialsToRemove = 0;
+			}
+			else
+			{
+				int inStack = entry.getValue().getAmount();
+				inventory.setItem(entry.getKey(), new ItemStack(Material.AIR, 0));
+				materialsToRemove -= inStack;
+			}
+		}
+		
+		return materialsToRemove == 0;
+	}
 
 	/**
 	 * Checks if players are in range, applies effects to them
@@ -167,10 +239,14 @@ public class BetterBeacons {
 
     // Mutable member variables
 	private BetterBeaconsProperties properties;
+	private Inventory inventory;
 	private Integer xMin;
 	private Integer xMax;
 	private Integer yMin;
 	private Integer yMax;
 	private Integer zMin;
 	private Integer zMax;
+	
+	//TODO: Change these values to config loading values!
+	private final int inventoryRows = 3;
 }
